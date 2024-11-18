454d29f6f2a249ec1824b56d023ad486e17ac9f8
samanthwest <samanthawest@ucsb.edu>
2024-11-17 20:16:40 -0800

    fix issues

--- plant-crossing/jest-setup.js
+++ plant-crossing/jest-setup.js
@@ -1,8 +1,7 @@
 import '@testing-library/jest-native/extend-expect';
 import 'react-native-gesture-handler/jestSetup';
 
-// Mock React Native's NativeAnimatedHelper
-jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+// jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 // Polyfill for TextEncoder/TextDecoder
 global.TextEncoder = require('util').TextEncoder;
