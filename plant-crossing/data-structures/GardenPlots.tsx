705435212697359a890630aa205017afadc5a9f2
Esme <esmepuzio@ucsb.edu>
2024-11-25 13:23:40 -0800

    watering animation added, gif doesn't always start at frame 0

--- plant-crossing/data-structures/GardenPlots.tsx
+++ plant-crossing/data-structures/GardenPlots.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import { PlantService } from "../managers/PlantService";
 import { PlotService } from "../managers/PlotService";
 import { SeedService } from "../managers/SeedService";
-import { View, StyleSheet, FlatList, Text, Dimensions, TouchableOpacity, ImageBackground, ImageSourcePropType } from 'react-native';
+import { View, StyleSheet, FlatList, Text, Dimensions, TouchableOpacity, ImageBackground, ImageSourcePropType, Animated, Image } from 'react-native';
 import {FIREBASE_AUTH, FIRESTORE_DB } from '../FirebaseConfig';
 import { arrayUnion, collection, onSnapshot, doc, getDoc, getDocs, getFirestore, updateDoc } from 'firebase/firestore';
 import { GardenTool } from "./GardenTools";
@@ -62,6 +62,11 @@ export interface GardenGridProps {
   
 export const GardenGrid = ({ selectedItem, setSelectedItem, onSeedPlanted }: GardenGridProps) => {
     const [plots, setPlots] = useState(playerGarden.getPlots());
+    const [isWatering, setIsWatering] = useState(false);
+    const [animationLocation, setAnimationLocation] = useState(-1); //plot that should be animated
+    // const [gifKey, setGifKey] = useState(0); // state to restart gif from frame 1 when animation plays
+    const [gifKey, setGifKey] = useState(''); 
+
     const userId = FIREBASE_AUTH.currentUser?.uid;
 
     const fetchPlots = async () => {
@@ -84,6 +89,23 @@ export const GardenGrid = ({ selectedItem, setSelectedItem, onSeedPlanted }: Gar
         fetchPlots();
     }, []);
 
+    const startWateringAnimation = (plotLocation: number) => {
+        setIsWatering(true);
+        setAnimationLocation(plotLocation);
+        restartGif();
+        const timeout = setTimeout(() => {
+            setIsWatering(false);
+            setAnimationLocation(-1);
+        }, 1250); // 1.25 second watering (5 frames, each display .25 seconds)
+        return () => {
+            clearTimeout(timeout);
+        };
+    };
+
+    const restartGif = () => {
+        setGifKey((prevKey) => (prevKey + 1)); // Increment key to re-render the GIF
+    };
+
     const handlePress = async (plot: Plot, index: number) => {
         if (!plot.unlocked) {
           await PlotService.unlockPlot(userId!, plot.location);
@@ -93,10 +115,10 @@ export const GardenGrid = ({ selectedItem, setSelectedItem, onSeedPlanted }: Gar
                 await SeedService.addSeed(new Seed(plot.plant.type, plot.plant.rarity, plot.plant.growthTime, plot.plant.maxWater, plot.plant.spriteNumber));
                 await PlotService.removePlantFromPlot(userId!, plot.location);
             } else if (selectedItem?.type == "WateringCan") {
-                console.log("selected wc");
                 let plantID = await PlantService.getPlantIdByDescription(plot.plant.type, plot.plant.rarity);
                 if(plantID){
                     await PlantService.waterPlant(plantID, 1);
+                    startWateringAnimation(plot.location);
                 }
             }
           } else if (selectedItem && (selectedItem.type != "WateringCan" && selectedItem.type != "Shovel")) {
@@ -110,8 +132,8 @@ export const GardenGrid = ({ selectedItem, setSelectedItem, onSeedPlanted }: Gar
     };
 
     const renderPlotContent = (plot: Plot) => {
-        if (plot.getUnlocked()) {
-            if (plot.getSeed()) {
+        if (plot.unlocked) {
+            if (plot.plant) {
                 // Planted plot with seed
                 return (
                     <ImageBackground 
@@ -119,9 +141,18 @@ export const GardenGrid = ({ selectedItem, setSelectedItem, onSeedPlanted }: Gar
                         style={styles.plotItem}
                         resizeMode="cover"
                     >
+                    {isWatering && (plot.location == animationLocation) ? (
+                        <Image 
+                            source={require('../assets/watering-animation/watering.gif')}
+                            style={styles.wateringGif} 
+                            resizeMode="center" 
+                            key={gifKey}
+                        />                
+                    ) : (
                         <View style={styles.plantOverlay}>
-                            <Text style={styles.plotText}>{plot.getSeed().getType()}</Text>
+                            <Text style={styles.plotText}>{plot.plant.type}</Text>
                         </View>
+                    )}
                     </ImageBackground>
                 );
             } else {
@@ -155,36 +186,15 @@ export const GardenGrid = ({ selectedItem, setSelectedItem, onSeedPlanted }: Gar
     return (
         <View style={styles.gridContainer}>
             <View style={styles.grid}>
-                {plots.map((plot, index) => {
-                    let content;
-                    if (plot.unlocked) {
-                        if (plot.plant) {
-                            content = (
-                                <View style={styles.plotItem}>
-                                    <Text style={styles.plotText}>{plot.plant?.type}</Text>
-                                </View>
-                            );
-                        } else {
-                            content = <View style={styles.emptyPlot} />;
-                        }
-                    } else {
-                        content = (
-                            <View style={styles.lockedPlot}>
-                                <Text style={styles.lockedText}>Locked</Text>
-                            </View>
-                        );
-                    }
-
-                    return (
-                        <TouchableOpacity
-                            key={`plot-${index}`}
-                            onPress={() => handlePress(plot, index)}
-                            style={styles.plotTouchable}
-                        >
-                            {content}
-                        </TouchableOpacity>
-                    );
-                })}
+                {plots.map((plot, index) => (
+                    <TouchableOpacity
+                        key={`plot-${index}`}
+                        onPress={() => handlePress(plot, index)}
+                        style={styles.plotTouchable}
+                    >
+                        {renderPlotContent(plot)}
+                    </TouchableOpacity>
+                ))}
             </View>
         </View>
     );
@@ -265,6 +275,14 @@ const styles = StyleSheet.create({
         backgroundColor: '#abf333',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    wateringGif: {
+        flex: 1,
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: plotSize,
+        width: plotSize
     },
     emptyPlot: {
         flex: 1,
