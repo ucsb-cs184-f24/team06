8f4cd86b09fe6a1ded46122da5ecabe9d49f2966
Esme <esmepuzio@ucsb.edu>
2024-12-06 04:47:42 -0800

    Added 2 more plants, removed unused sprites, shuffled rarity levels

--- plant-crossing/data/items.ts
+++ plant-crossing/data/items.ts
@@ -7,26 +7,28 @@ export const availableSeeds = [
   new Seed("violet", Rarity.common, 4, 5),
   new Seed("sunflower", Rarity.common, 5, 6),
   new Seed("shrub", Rarity.common, 5, 5),
-  new Seed("tropical_tree", Rarity.common, 5, 5),
   new Seed("reed", Rarity.common, 5, 5),
 
   // uncommon
-  new Seed("wildflower", Rarity.uncommon, 6, 6),
+  new Seed("aloe vera", Rarity.uncommon, 6, 6),
   new Seed("poppy", Rarity.uncommon, 6, 6),
-  //new Seed("yellow_cactus", Rarity.uncommon, 6, 6),
-
+  new Seed("pink_cactus", Rarity.uncommon, 6, 10),
+  new Seed("wildflower", Rarity.uncommon, 6, 6),
+  
   // rare
   new Seed("tulip", Rarity.rare, 7, 7),
-  //new Seed("short_cactus", Rarity.rare, 7, 7),
+  new Seed("short_cactus", Rarity.rare, 7, 10),
+  new Seed("tropical_tree", Rarity.rare, 7, 10),
+  new Seed("oak_tree", Rarity.rare, 7, 7),
 
   // unique
   new Seed("cherry_blossom", Rarity.unique, 8, 8),
   new Seed("chrysanthemum", Rarity.unique, 8, 8),
-  //new Seed("tall_cactus", Rarity.unique, 8, 8),
+  new Seed("tall_cactus", Rarity.unique, 8, 10),
 
   // legendary
   new Seed("chromafruit", Rarity.legendary, 9, 9),
-  //new Seed("pink_cactus", Rarity.legendary, 9, 9),
+  new Seed("golden_cactus", Rarity.uncommon, 9, 10),
 ];
 
 export const startingSeeds = availableSeeds.filter(
